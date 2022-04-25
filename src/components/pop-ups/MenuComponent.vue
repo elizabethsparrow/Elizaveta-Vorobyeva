9542d884455baa02feeb5a0a34d6c941dcae60bd
elizabethsparrow <vliza162@gmailcom>
2022-04-25 11:52:06 +0300

    added files of pop-up windows

--- src/components/pop-ups/MenuComponent.vue
+++ src/components/pop-ups/MenuComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="free-consultation">
+  <div class="navigation-menu-pop-up">
 
   </div>
 </template>
--- src/components/pop-ups/WorkDescriptionComponent.vue
+++ src/components/pop-ups/WorkDescriptionComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="free-consultation">
+  <div class="work-descr-pop-up">
 
   </div>
 </template>
